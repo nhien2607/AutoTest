11d694eee07cb8b9e41ecf52fd11b054d765814a
nhien2607 <nhienledokhanh@gmail.com>
2024-02-28 20:53:01 +0700

    day11

--- src/main/java/automation/constant/CT_Common.java
+++ src/main/java/automation/constant/CT_Common.java
@@ -5,4 +5,6 @@ public class CT_Common {
 	public static String URL_SelectorHub = "https://selectorshub.com/xpath-practice-page/";
 	public static String URL_Alada = "https://alada.vn/tai-khoan/dang-ky.html";
 	public static String URL_Automationfc = "https://automationfc.github.io/basic-form/index.html";
+	public static String checkboxSeleniumEasy = "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
+	public static String radioButton = "http://demo.seleniumeasy.com/basic-radiobutton-demo.html";
 }
